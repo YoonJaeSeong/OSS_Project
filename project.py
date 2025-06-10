68cfa4530eb96524435404793b505a7b15319399
rr <yjs10210919@gmail,com>
2025-06-10 15:51:56 +0900

    load_save

--- project.py
+++ project.py
@@ -1,2 +1,10 @@
-test = 1
-print(test)
+#파일 입출력력
+def load_contacts():
+    if not os.path.exists(CONTACT_FILE):
+        return []
+    with open(CONTACT_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_contacts(contacts):
+    with open(CONTACT_FILE, "w", encoding="utf-8") as f:
+        json.dump(contacts, f, ensure_ascii=False, indent=4)
