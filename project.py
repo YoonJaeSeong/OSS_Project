cabc318a1861f6ac8c31e44f3dfffc93e48fc095
rr <yjs10210919@gmail,com>
2025-06-10 15:45:33 +0900

    edit

--- project.py
+++ project.py
@@ -1,2 +1,16 @@
-test = 1
-print(test)
+#연락처 수정
+def load_contacts():
+    pass
+
+def save_contacts():
+    pass
+
+
+def edit_contact(old_name, new_name, new_phone):
+    contacts = load_contacts()
+    for contact in contacts:
+        if contact['name'] == old_name:
+            contact['name'] = new_name
+            contact['phone'] = new_phone
+            break
+    save_contacts(contacts)
