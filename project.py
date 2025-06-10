88c80900dfa8fefd6b565c5b16cb53f3291cf0a1
rr <yjs10210919@gmail,com>
2025-06-10 15:42:09 +0900

    list

--- project.py
+++ project.py
@@ -1,2 +1,7 @@
-test = 1
-print(test)
+#연락처 목록
+def load_contacts():
+    pass
+def list_contacts():
+    contacts = load_contacts()
+    for i, contact in enumerate(contacts, 1):
+        print(f"{i}. {contact['name']} - {contact['phone']}")
