ed631a536eb2c540f9db47856519182167eb2091
rr <yjs10210919@gmail,com>
2025-06-10 15:49:21 +0900

    search

--- project.py
+++ project.py
@@ -1,2 +1,6 @@
-test = 1
-print(test)
+#연락처 검색색
+def search_contact(keyword):
+    contacts = load_contacts()
+    results = [c for c in contacts if keyword.lower() in c['name'].lower()]
+    for contact in results:
+        print(f"{contact['name']} - {contact['phone']}")
